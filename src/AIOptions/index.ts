7ebe58a8f6f328c6af25624b279d2b343f02c7bf
Caio Eduardo Justo Mendonça <caioeduardojm4@gmail.com>
2023-01-31 17:22:13 -0400

    rewrite

---
+++ src/AIOptions/index.ts
@@ -0,0 +1,15 @@
+export const OptionQA = {
+    name: "Noomi responde",
+    id: "q&a",
+    description: "Responda as perguntas com base no conhecimento existente",
+    option: {
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 1500,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    }
+  }
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,76 @@
-import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
-import OptionSelection from "./components/OptionSelection";
-import Translation from "./components/Translation";
-import { arrayItems } from "./AIOptions";
-import { useState } from "react";
-
+import { OptionQA} from "./AIOptions";
+import { useEffect, useState } from "react";
+import { Box, Button, TextField, Typography, CircularProgress } from "@mui/material";
 function App() {
   const configuration = new Configuration({
     apiKey: import.meta.env.VITE_Open_AI_Key,
   });
   const openai = new OpenAIApi(configuration);
-  const [option, setOption] = useState({});
+  const option = OptionQA.option;
   const [result, setResult] = useState("");
   const [input, setInput] = useState("");
+  const [ loadgin, setLoading] = useState(false);
   // console.log(import.meta.env.VITE_Open_AI_Key);
-  const selectOption = (option) => {
-    setOption(option);
-  };
+
+  useEffect(()=>{
+    if(result){
+      setLoading(false)
+    }
+  }, [result])
 
   const doStuff = async () => {
     let object = { ...option, prompt: input };
-
+    setLoading(true)
     const response = await openai.createCompletion(object);
-
     setResult(response.data.choices[0].text);
   };
 
   return (
-    <div className="App">
-      {Object.values(option).length === 0 ? (
-        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
-      ) : (
-        <Translation doStuff={doStuff} setInput={setInput} result={result} />
-      )}
-    </div>
+    <body>
+      <Box sx={{  width: '100%', margin: 0, padding: 0}}>
+        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', marginTop: 2, marginBottom: 2}}>
+          <img src="/logo.png" style={{ width: '80%', maxWidth: '350px'}}/>  
+        </Box>
+
+        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%'}}>
+          <TextField
+            rows={4}
+            type="text"
+            multiline
+            label="Faça sua pergunta:"
+            sx={{ width: '80%' }}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </Box>
+        <Box sx={{justifyContent: 'center', alignItems: 'center', display: 'flex', marginTop: 2}}> 
+          <Button 
+          variant="contained"
+          style={{backgroundColor: loadgin ?'#c8c8c8':'#16315C', color: loadgin ? '#16315C' : '#fff'}}
+          disabled={loadgin}
+          onClick={doStuff}>
+            Noomi responde
+          </Button>
+        </Box>
+
+        <Box sx={{justifyContent: 'center', alignItems: 'center', display: 'flex', marginTop: 2, width: '100%'}}>
+            { !loadgin ? (
+              <Box>
+                <Typography 
+                sx={{justifyContent: 'center', alignItems: 'center', display: 'flex',margin: 'auto', width: '70%', textAlign: 'center'}}
+                variant="h6"
+                component="p"
+                color="grey.600"
+                >{result.length > 0 ? result : ""}
+                </Typography>
+              </Box>
+            ) : (    
+            <Box sx={{ display: 'flex' }}>
+            <CircularProgress />
+            </Box>)}
+          </Box>
+      </Box>
+      </body>
   );
 }
 
