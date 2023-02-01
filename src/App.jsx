df2b4ddcdf9d2b6d810276317a0a2fb5677c7cac
Caio Eduardo Justo Mendonça <caioeduardojm4@gmail.com>
2023-02-01 17:27:12 -0400

    edit margin

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ function App() {
     <body>
       <Box sx={{  width: '90vw', height:'90vh', margin: 'auto', padding: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '1000px'}}>
         <Box sx={{  width: smDown ? '100%' : '80%'}}>
-        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', marginTop: smDown ? 10 : 5, marginBottom: smDown ? 5 : 10}}>
+        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', marginTop: smDown ? 10 : 5, marginBottom: 5}}>
           <img src="/logo.png" style={{ width: '70%', maxWidth: '300px', maxHeight: '300px'}}/>  
         </Box>
         { messages.length > 0 ? (
