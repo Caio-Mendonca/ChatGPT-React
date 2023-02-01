03a5accc5d966f305de6b4015354d9601fd2c40e
Caio Eduardo Justo Mendonça <caioeduardojm4@gmail.com>
2023-02-01 17:33:16 -0400

    altered marginBottom in device

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
     <body>
-      <Box sx={{  width: '90vw', height: smDown ? '85vh': '90vh', margin: 'auto', padding: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '1000px'}}>
+      <Box sx={{  width: '90vw', height: '90vh', margin: 'auto', padding: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '1000px'}}>
         <Box sx={{  width: smDown ? '100%' : '80%'}}>
         <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', marginTop: smDown ? 10 : 5, marginBottom: 5}}>
           <img src="/logo.png" style={{ width: '70%', maxWidth: '300px', maxHeight: '300px'}}/>  
@@ -149,7 +149,7 @@ function App() {
           <Button  variant="outlined" color="secondary" sx={{marginBottom: 2}} onClick={()=> handleCleanMessages()}>Limpar histórico</Button>
         ) : (<></>)}
         </Box>
-        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', borderRadius: '10px'}}>
+        <Box sx={{ justifyContent: 'center', alignItems: 'center', display: 'flex', width: '100%', borderRadius: '10px', marginBottom: smDown ? 2 : 1}}>
           <TextField
             type="text"
             color="primary"
